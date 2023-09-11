9eda24a1da9df586012928a2904627777d93163d
Hiroshi Atsuta <atsuta@ieee.org>
2023-09-11 15:49:15 +0900

    refactor: Refactor test cases

--- tests/test_weights.py
+++ tests/test_weights.py
@@ -4,21 +4,10 @@ from rebasicspy.weights import initialize_weights
 from scipy.sparse import coo_matrix, csc_matrix, csr_matrix
 
 
-def test_initialize_weights_get_ndarray():
-    weights = initialize_weights((3, 3), sparsity_type="dense")
-    assert isinstance(weights, np.ndarray)
-
-
-def test_initialize_weights_get_csr():
-    weights = initialize_weights((3, 3), sparsity_type="csr")
-    assert isinstance(weights, csr_matrix)
-
-
-def test_initialize_weights_get_csc():
-    weights = initialize_weights((3, 3), sparsity_type="csc")
-    assert isinstance(weights, csc_matrix)
-
-
-def test_initialize_weights_get_coo():
-    weights = initialize_weights((3, 3), sparsity_type="coo")
-    assert isinstance(weights, coo_matrix)
+@pytest.mark.parametrize(
+    "sparsity_type,expected_type",
+    [("dense", np.ndarray), ("csr", csr_matrix), ("csc", csc_matrix), ("coo", coo_matrix)],
+)
+def test_initialize_weights_specify_sparsity_type(sparsity_type, expected_type):
+    w = initialize_weights((5, 5), sparsity_type=sparsity_type)
+    assert isinstance(w, expected_type)
