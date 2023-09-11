ecaeccf5275ea16f59d99fe80681e5007aedb6ce
Hiroshi Atsuta <atsuta@ieee.org>
2023-09-11 16:00:04 +0900

    test: Add test for weights connectivity

--- tests/test_weights.py
+++ tests/test_weights.py
@@ -11,3 +11,19 @@ from scipy.sparse import coo_matrix, csc_matrix, csr_matrix
 def test_initialize_weights_specify_sparsity_type(sparsity_type, expected_type):
     w = initialize_weights((5, 5), sparsity_type=sparsity_type)
     assert isinstance(w, expected_type)
+
+
+@pytest.mark.parametrize(
+    "sparsity_type,shape,connectivity", [("coo", (10, 10), 0.02), ("csr", (10, 10), 0.05), ("csc", (10, 10), 0.15)]
+)
+def test_initialize_weights_specify_connectivity(sparsity_type, shape: tuple[int, int], connectivity: float):
+    w = initialize_weights(shape, sparsity_type=sparsity_type, connectivity=connectivity)
+    actual = w.nnz / w.toarray().size
+    assert actual == connectivity
+
+
+@pytest.mark.parametrize("shape,connectivity", [((10, 10), 0.02), ((10, 10), 0.05), ((10, 10), 0.15)])
+def test_initialize_weights_specify_connectivity_with_dense_array(shape: tuple[int, int], connectivity: float):
+    w = initialize_weights(shape, sparsity_type="dense", connectivity=connectivity)
+    actual = np.count_nonzero(w) / w.size
+    assert actual == connectivity
