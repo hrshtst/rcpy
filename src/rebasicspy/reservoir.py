71f12b08b98e3d9064fc667b48d79c9ae20c059d
Hiroshi Atsuta <atsuta@ieee.org>
2023-09-12 18:25:29 +0900

    feat: Add Reservoir class

---
+++ src/rebasicspy/reservoir.py
@@ -0,0 +1,76 @@
+import copy
+from dataclasses import dataclass
+from typing import Callable, Iterable
+
+from rebasicspy._type import SparsityType, WeightsType
+from rebasicspy.weights import initialize_weights, uniform
+
+
+@dataclass
+class ReservoirBuilder:
+    size: int
+    spectral_radius: float
+    connectivity: float
+    leaking_rate: float
+    W_init: Callable[..., WeightsType] = uniform
+    Win_init: Callable[..., WeightsType] = uniform
+    input_scaling: float | Iterable[float] = 1.0
+    add_bias: bool = True
+    bias_scaling: float = 1.0
+    seed: int | None = None
+
+
+class Reservoir(object):
+    _builder: ReservoirBuilder
+    _W: WeightsType
+    _Win: WeightsType
+    _leaking_rate: float
+    _seed: int | None
+
+    def __init__(self, builder: ReservoirBuilder):
+        self._builder = copy.copy(builder)
+        self._leaking_rate = self._builder.leaking_rate
+        self._seed = self._builder.seed
+        self.initialize_internal_weights()
+
+    @property
+    def W(self) -> WeightsType:
+        return self._W
+
+    @property
+    def Win(self) -> WeightsType:
+        return self._Win
+
+    @property
+    def leaking_rate(self) -> float:
+        return self._leaking_rate
+
+    def initialize_internal_weights(
+        self,
+        size: int | None = None,
+        spectral_radius: float | None = None,
+        connectivity: float | None = None,
+        W_init: Callable[..., WeightsType] | None = None,
+        sparsity_type: SparsityType = "csr",
+        seed: int | None = None,
+    ) -> WeightsType:
+        if size is None:
+            size = self._builder.size
+        if spectral_radius is None:
+            spectral_radius = self._builder.spectral_radius
+        if connectivity is None:
+            connectivity = self._builder.connectivity
+        if W_init is None:
+            W_init = self._builder.W_init
+        if seed is None:
+            seed = self._builder.seed
+
+        self._W = initialize_weights(
+            (size, size),
+            W_init,
+            spectral_radius=spectral_radius,
+            connectivity=connectivity,
+            sparsity_type=sparsity_type,
+            seed=seed,
+        )
+        return self._W
---
+++ tests/test_reservoir.py
@@ -0,0 +1,84 @@
+import numpy as np
+import pytest
+from numpy.testing import assert_array_equal
+from rebasicspy.metrics import WeightsType
+from rebasicspy.reservoir import Reservoir, ReservoirBuilder
+from rebasicspy.weights import bernoulli, normal, uniform
+from scipy.sparse import csc_matrix, csr_matrix
+
+
+def get_default_builder() -> ReservoirBuilder:
+    return ReservoirBuilder(size=15, spectral_radius=0.95, connectivity=0.2, leaking_rate=0.98)
+
+
+@pytest.fixture
+def default_builder() -> ReservoirBuilder:
+    return get_default_builder()
+
+
+@pytest.fixture
+def default_reservoir() -> Reservoir:
+    return Reservoir(get_default_builder())
+
+
+def actual_spectral_radius(w: WeightsType) -> float:
+    if isinstance(w, np.ndarray):
+        return max(abs(np.linalg.eigvals(w)))
+    else:
+        return max(abs(np.linalg.eigvals(w.toarray())))
+
+
+def actual_connectivity(w: WeightsType) -> float:
+    if isinstance(w, np.ndarray):
+        return np.count_nonzero(w) / (w.shape[0] * w.shape[1])
+    else:
+        return w.nnz / (w.shape[0] * w.shape[1])
+
+
+def ensure_ndarray(w: WeightsType) -> np.ndarray:
+    if isinstance(w, np.ndarray):
+        return w
+    else:
+        return w.toarray()
+
+
+class TestReservoir:
+    def test_init_builder_should_be_copied(self, default_builder: ReservoirBuilder):
+        res = Reservoir(default_builder)
+        assert res._builder is not default_builder
+
+    def test_init_leaking_rate(self, default_reservoir: Reservoir):
+        assert default_reservoir.leaking_rate == 0.98
+
+    def test_init_seed(self, default_reservoir: Reservoir):
+        assert default_reservoir._seed is None
+
+    def test_initialize_internal_weights(self, default_reservoir: Reservoir):
+        W = default_reservoir.W
+        assert W.shape == (15, 15)
+
+        expected_spectral_radius = default_reservoir._builder.spectral_radius
+        assert actual_spectral_radius(W) == pytest.approx(expected_spectral_radius)
+
+        expected_connectivity = default_reservoir._builder.connectivity
+        assert actual_connectivity(W) == expected_connectivity
+
+        assert type(W) is csr_matrix
+
+    @pytest.mark.parametrize(
+        "size,sr,p,dist,sparsity,expected_type",
+        [(10, 1.3, 0.15, normal, "dense", np.ndarray), (20, 0.9, 0.1, uniform, "csc", csc_matrix)],
+    )
+    def test_initialize_internal_weights_reinitialize(
+        self, default_reservoir: Reservoir, size, sr, p, dist, sparsity, expected_type
+    ):
+        W0_array = ensure_ndarray(default_reservoir.W)
+        W = default_reservoir.initialize_internal_weights(
+            size=size, spectral_radius=sr, connectivity=p, W_init=dist, sparsity_type=sparsity
+        )
+        assert W.shape == (size, size)
+        assert actual_spectral_radius(W) == pytest.approx(sr)
+        assert actual_connectivity(W) == p
+        assert type(W) is expected_type
+        with pytest.raises(AssertionError):
+            assert_array_equal(W0_array, ensure_ndarray(W))
