fix: Correct calculation of bias scaling

src/rebasicspy/weights.py
@@ -313,17 +313,17 @@ def _scale_inputs(weights: WeightsType, scaling: float | Iterable[float]) -> Wei
         weights_arr = weights.copy()
     if len(scaling_arr) == 2:
         # When two values are given as the input scaling, the first
-        # element is multiplied by the first element of the weights as
+        # element is multiplied by the first column of the weights as
         # it is assumed to be corresponding to the input bias. Then
         # the second element of the input scaling is multiplied by the
         # rest of the weights.
-        scaled_w_bias = weights_arr[0:1] * scaling_arr[0]
-        scaled_w_rest = weights_arr[1:] * scaling_arr[1]
-        return np.vstack((scaled_w_bias, scaled_w_rest))
-    elif len(scaling_arr) == len(weights_arr):
+        scaled_w_bias = weights_arr[:, :1] * scaling_arr[0]
+        scaled_w_rest = weights_arr[:, 1:] * scaling_arr[1]
+        return np.hstack((scaled_w_bias, scaled_w_rest))
+    elif len(scaling_arr) == weights_arr.shape[1]:
         # When more than two values are given as the input scaling,
         # each element is multiplied to the weights in the
         # element-wise way.
-        return weights_arr * scaling_arr.reshape(weights_arr.shape)
+        return weights_arr * scaling_arr
     else:
         raise ValueError(f"The size of `scaling` is mismatched with `weights`.")

tests/test_weights.py
@@ -150,23 +150,28 @@ def test_initialize_weights_scale_inputs(scaling: float):
 
 @pytest.mark.parametrize("scaling", [(0.1, 0.5), (2.0, 5.0)])
 def test_initialize_weights_when_two_scalings_given_they_applied_first_and_rest(scaling: tuple[float, float]):
+    reservoir_size = 5
+    input_dim = 2
     # When two values are given as the scaling input,
-    w = initialize_weights((5, 1), ones, scaling=scaling, sparsity_type="dense")
-    # The first element (corresponding to the bias) of the initial
+    w = initialize_weights((reservoir_size, input_dim + 1), ones, scaling=scaling, sparsity_type="dense")
+    # The first column (corresponding to the bias) of the initial
     # weights is multiplied by the first of scaling.
-    assert_array_equal(w[0], np.array([scaling[0]]))
+    assert_array_equal(w[:, 0], np.ones(reservoir_size) * scaling[0])
     # The rest of the initial weights are multiplied by the second of
     # scaling.
-    assert_array_equal(w[1:], np.full((4, 1), scaling[1]))
+    assert_array_equal(w[:, 1:], np.ones((reservoir_size, input_dim)) * scaling[1])
 
 
 def test_initialize_weights_when_more_than_two_scalings_given_applied_elementwise():
+    reservoir_size = 5
+    input_dim = 2
     # When more than two values are given as the scaling input,
-    scaling = np.arange(5)
-    w = initialize_weights((5, 1), ones, scaling=scaling, sparsity_type="dense")
+    scaling = np.arange(input_dim + 1)
+    w = initialize_weights((reservoir_size, input_dim + 1), ones, scaling=scaling, sparsity_type="dense")
     # The elements of the scaling input are multiplied by the initial
     # weights in the element-wise way.
-    assert_array_equal(w, np.arange(5).reshape(-1, 1))
+    expected = np.repeat(np.atleast_2d(np.arange(input_dim + 1)), reservoir_size, axis=0)
+    assert_array_equal(w, expected)
 
 
 def test_initialize_weights_raise_exception_when_shape_of_scaling_incorrect():