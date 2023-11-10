6425275533715c8ccc2f0d48ea8e8a9ee67f00e7
Hiroshi Atsuta <atsuta@ieee.org>
2023-11-10 17:06:27 +0900

    feat: Add RLS class

---
+++ src/rebasicspy/rls.py
@@ -0,0 +1,78 @@
+import numpy as np
+
+from rebasicspy.readout import Readout, compute_error, initialize_weights, rescale_data
+
+
+class RLS(Readout):
+    _forgetting_factor: float
+    _delta: float
+    _Wout_init: str
+    _P: np.ndarray
+
+    def __init__(self, forgetting_factor: float = 0.98, delta: float = 0.001, Wout_init: str = "zeros"):
+        self.forgetting_factor = forgetting_factor
+        self.delta = delta
+        self._Wout_init = Wout_init
+        super().__init__(batch_interval=1)
+
+    @property
+    def forgetting_factor(self) -> float:
+        return self._forgetting_factor
+
+    @forgetting_factor.setter
+    def forgetting_factor(self, lr: float) -> float:
+        self._forgetting_factor = lr
+        return self._forgetting_factor
+
+    @property
+    def delta(self) -> float:
+        return self._delta
+
+    @delta.setter
+    def delta(self, delta: float) -> float:
+        self._delta = delta
+        return self._delta
+
+    def process_backward(self, x: np.ndarray, y_target: float | int | np.ndarray, sample_weight: float | int | None):
+        # Rescale data
+        x, y_target = rescale_data(x, y_target, sample_weight)
+
+        # Error estimation
+        try:
+            e = compute_error(self, x, y_target)
+        except RuntimeError:
+            # Initialize Wout and inverse of autocorrelation matrix
+            self._Wout = initialize_weights((len(y_target), len(x)), initializer=self._Wout_init)
+            self._P = (1.0 / self.delta) * np.identity(len(x))
+            e = compute_error(self, x, y_target)
+
+        # Computation of gain vector
+        u = np.dot(self._P, x)
+        k = (1.0 / (self.forgetting_factor + np.dot(x, u))) * u
+
+        # Inverse of autocorrelation matrix update
+        self._P = (1.0 / self.forgetting_factor) * (self._P - np.outer(k, u))
+        # Make the inverse of autocorrelation matrix symmetrix to
+        # avoid numrical unstability.
+        tril = np.tril_indices(self._P.shape[0])
+        self._P[tril] = self._P.T[tril]
+
+        # Tap-weight matrix adaptation
+        dw = np.outer(e, k)
+        self._Wout += dw
+        return super().process_backward(x, y_target, sample_weight)
+
+    def process_backward_batch(self):
+        # Do nothing
+        return super().process_backward_batch()
+
+    def finalize_backward_batch(self):
+        # Do nothing
+        return super().finalize_backward_batch()
+
+    def finalize(self):
+        # Do nothing
+        pass
+
+    def reset(self):
+        return super().reset()
---
+++ tests/test_rls.py
@@ -0,0 +1,69 @@
+import numpy as np
+import pytest
+from numpy.testing import assert_array_almost_equal
+from rebasicspy.rls import RLS
+
+from ._base import LargeDataSet, SmallDataSet, SmallDataSetMO
+
+
+@pytest.fixture
+def rls() -> RLS:
+    return RLS()
+
+
+class TestRLS:
+    def test_init(self):
+        rls = RLS()
+        assert rls.forgetting_factor == 0.98
+        assert rls.delta == 0.001
+        assert rls.batch_interval == 1
+        assert rls._Wout_init == "zeros"
+
+    @pytest.mark.parametrize("lr,delta", [(1.0, 0.01), (0.01, 0.00001), (0.005, 0.0)])
+    def test_init_forgetting_factor(self, lr: float, delta: float):
+        rls = RLS(lr, delta)
+        assert rls.forgetting_factor == lr
+        assert rls.delta == delta
+
+    def test_process_backward_initialize_Wout_and_P(self, rls: RLS):
+        data = SmallDataSet()
+        rls.forgetting_factor = 0.98
+        x, y = next(iter(data))
+        rls.backward(x, y)
+        assert rls.Wout.shape == data.coef.shape
+        assert rls._P.shape == (len(x), len(x))
+
+    def test_process_backward_update_Wout_in_few_iterations(self, rls: RLS):
+        data = iter(SmallDataSet())
+        rls.forgetting_factor = 0.1
+        rls.delta = 0.001
+        # n = 1
+        x, y = next(data)
+        rls.backward(x, y)
+        w = 3000 / 2000.1
+        assert_array_almost_equal(np.array([[w, w]]), rls.Wout)
+        p = 1e6 / 2000.1
+        assert_array_almost_equal(10 * np.array([[1e3 - p, -p], [-p, 1e3 - p]]), rls._P)
+
+    def test_process_backward_update_Wout_in_few_iterations_multi_output(self, rls: RLS):
+        data = iter(SmallDataSetMO())
+        rls.forgetting_factor = 0.1
+        rls.delta = 0.001
+        # n = 1
+        x, y = next(data)
+        rls.backward(x, y)
+        w1 = 1e3 / 3000.1
+        w2 = 2e3 / 3000.1
+        assert_array_almost_equal(np.array([[w1, w1, w1], [w2, w2, w2]]), rls.Wout)
+        p = 1e6 / 3000.1
+        expected_P = 10 * np.array([[1e3 - p, -p, -p], [-p, 1e3 - p, -p], [-p, -p, 1e3 - p]])
+        assert_array_almost_equal(expected_P, rls._P)
+
+    def test_fit(self, rls: RLS):
+        data = LargeDataSet()
+        rls.forgetting_factor = 0.98
+        rls.delta = 1e-3
+        for x, y in data:
+            rls.backward(x, y)
+        rls.fit()
+        assert_array_almost_equal(data.coef, rls.Wout, decimal=1)
